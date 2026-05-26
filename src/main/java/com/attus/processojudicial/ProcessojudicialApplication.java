d8e39e65c07fa42ba3a893382957a035fbc3771e
Cauã Salgado <cauahenrique230503@gmail.com>
2026-05-26 14:35:16 -0300

    chore: Configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/attus/processojudicial/ProcessojudicialApplication.java
@@ -0,0 +1,13 @@
+package com.attus.processojudicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessojudicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessojudicialApplication.class, args);
+	}
+
+}
